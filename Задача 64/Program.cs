9188524d4512b3c3680d2b22327210fdbfafaea3
Anastasia <miss.markina2012@mail.rum>
2023-11-06 23:14:32 +0300

    Решение задачи 64

---
+++ Задача 64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        RecursivePrint(N);
+        Console.ReadKey();
+    }
+
+    static void RecursivePrint(int N)
+    {
+        if (N == 1) return;
+        Console.WriteLine(N);
+        RecursivePrint(N - 1);
+    }
+}
